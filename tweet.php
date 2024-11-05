01974a0019857c4c15d5d1590207d8f8c03c565b
Edwar404 <edwar.ibrahim1813336@gmail.com>
2024-11-05 10:01:15 +0200

    tampilan komen

--- tweet.php
+++ tweet.php
@@ -43,20 +43,63 @@ $queryPosting = mysqli_query($koneksi, "SELECT * FROM tweet WHERE id_user='$id_u
     </div>
     <div class="col-sm-12 mt-3">
         <?php while ($rowPosting = mysqli_fetch_array($queryPosting)) { ?>
-            <div class="d-flex">
-                <div class="flex-shrink-0">
+            <div class="d-flex ">
+                <div class="flex-shrink-0 d-flex gap-3">
                     <img src="upload/<?php echo !empty($rowUser['foto']) ? $rowUser['foto'] : 'https://placehold.co/800x200' ?>" alt="..." class="rounded-circle border border-2" height="50" width="50">
                 </div>
+                <div class="flex-grow-1 ms-3" style="max-width:500px;">
+                    <div class="d-flex gap-3">
+                        <p><?php echo $rowUser['nama_lengkap'] ?></p>
+                        <p><i></i>(<?php echo $rowUser['nama_pengguna'] ?>)</i></p>
+                    </div>
+                    <div class="d-flex">
+                        <?php if (!empty($rowPosting['foto'])): ?>
+                            <img class="rounded" src="upload/<?php echo $rowPosting['foto'] ?>" height="250" width="250" alt="">
+                        <?php endif ?>
+                        <div class="">
+                            <?php echo $rowPosting['content']; ?>
+                        </div>
+                    </div>
+                </div>
                 <div class="flex-grow-1 ms-3">
-                    <?php if (!empty($rowPosting['foto'])): ?>
-                        <img class="rounded-" src="upload/<?php echo $rowPosting['foto'] ?>" alt="">
-                    <?php endif ?>
-                    <?php echo $rowPosting['content']; ?>
+                    <form action="add_comment.php" id="comment-form" method="post">
+                        <input type="text" name="status_id" value="<?php echo $rowPosting['id'] ?>">
+                        <input type="text" name="user_id" value="<?php echo $rowPosting['id_user'] ?>">
+
+                        <textarea name="comment_text" class="form-control" id="comment_text" cols="5" rows="5" placeholder="Tulis Balasan Anda ... "></textarea>
+                        <button class="btn btn-primary btn-sm mt-2" type="submit">Kirim Balasan</button>
+                    </form>
+
+                    <div class="alert mt-2" id="comment-alert" style="display: none;"></div>
+                    <div class="mt-1">
+                        <?php
+                        if (isset($rowPosting['comment_text'])) {
+                            $idStatus = $rowPosting['status_id'];
+                            $userId = $rowPosting['user_id'];
+                            $queryComment = mysqli_query($koneksi, "SELECT * FROM comments WHERE ststus_id = $idStatus AND user_id = $userId");
+                            $rowCounts = mysqli_fetch_all($queryComment, MYSQLI_ASSOC);
+
+                            foreach ($rowCounts as $rowCount) {
+                        ?>
+                                <span>
+                                    <pre>Komentar : <?php echo $rowCount['comment_text'] ?></pre>
+                                </span>
+                                <!-- <div class="d-flex gap-3">
+                                    <p><?php echo $rowComment['comment_text'] ?></p>
+                                    <p><i></i>(<?php echo $rowComment['nama_lengkap'] ?>)</p>
+                                    <p><i></i>(<?php echo $rowComment['nama_pengguna'] ?>)</p>
+                                    <p><small><?php echo $rowComment['tanggal'] ?></small></p>
+                                </div> -->
+                        <?php
+                            }
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
+            <hr>
         <?php } ?>
     </div>
-    <hr>
 </div>
 
 <!-- MODAL TWEET -->
